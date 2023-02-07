e204a6cbd9482be0c18d56c76ec28c35b9334948
valo <valorapaulinio@gmail.com>
2023-02-07 16:06:40 +0300

    initial commit

---
+++ src/test/java/postmanTest.java
@@ -0,0 +1,31 @@
+import io.restassured.filter.log.LogDetail;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.Argument;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.equalTo;
+
+public class postmanTest {
+
+    @Test
+    public void test1() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+    }
+
+
+    }

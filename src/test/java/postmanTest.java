1503e85e5266da7c29a78a1481b642f442a4d8de
valo <valorapaulinio@gmail.com>
2023-02-07 16:21:56 +0300

    false

--- src/test/java/postmanTest.java
+++ src/test/java/postmanTest.java
@@ -23,7 +23,7 @@ public class postmanTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"))
+                .body("data", equalTo("some dato"))
         ;
     }
 
